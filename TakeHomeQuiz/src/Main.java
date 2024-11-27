49e28834deecc18050ded4ab3c775ae7fa84908b
sahanamohankumar <sahanamohankumar23@gmail.com>
2024-11-26 21:14:05 -0500

    Add functional interface for range sum

--- TakeHomeQuiz/src/Main.java
+++ TakeHomeQuiz/src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    @FunctionalInterface
+    public interface Sum{
+        int sum(int a, int b);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
