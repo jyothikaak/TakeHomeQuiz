2b03784c4746b3be642988eb44aaf1b99213fc4c
Jyothika <88742071+jyothikaak@users.noreply.github.com>
2024-11-27 10:49:30 -0500

    Delete TakeHomeClone directory

--- TakeHomeClone/src/Main.java
+++
@@ -1,6 +0,0 @@
-//Jyothika Akkineni and Sahana Mohankumar
-public class Main{
-public static void main(String[] args) {
-    System.out.println("Hello World!"); //comment to create merge conflict
-}
-}
